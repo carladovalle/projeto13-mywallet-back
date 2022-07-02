19c4377498aed83f5ff743698a1d782cc7adf5de
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-07-02 13:41:33 -0300

    Coloca post e get de novas saídas

--- index.js
+++ index.js
@@ -146,6 +146,66 @@ app.post('/input', async (req,res) => {
 
 })
 
+app.get('/output', async (req,res) => {
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    const session = await db.collection('contas').findOne({ token });
+
+    if (!session) {
+        return res.sendStatus(401);
+    }
+    
+    try {
+    
+        const contas = await db.collection('contas').find({ user1Id: new ObjectId(session.user1Id) }).toArray();
+    
+        res.send(contas);
+        res.send(authorization);
+
+    } catch (error) {
+        
+        return res.status(500).send("Deu ruim");
+   
+    }
+
+})
+
+app.post('/output', async (req,res) => {
+
+    const conta = req.body;
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+
+    const contaSchema = joi.object({
+        valor: joi.number().required(),
+        descricao: joi.string().required()
+    })
+
+    const { error } = contaSchema.validate(conta);
+
+    if (error) {
+        return res.sendStatus(422);
+    }
+
+    try {
+        const session = await db.collection('contas').findOne({ token });
+
+        if (!session) {
+            return res.sendStatus(401);
+        }
+    
+        await db.collection('contas').insertOne({ ... conta, user1Id: session.user1Id });
+        return res.status(201).send('Valor adicionado com sucesso');
+
+    } catch (error) {
+        
+        return res.status(500).send("Deu ruim");
+   
+    }
+
+})
+
 app.listen(PORT, () => {
     console.log(chalk.blue.bold(`Servidor rodando na porta ${PORT}`))
 })
