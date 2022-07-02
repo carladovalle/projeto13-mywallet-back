ef92d7712113c2d8517d6d3427394e5708ef88b4
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-07-02 13:39:23 -0300

    Coloca post e get de novas entradas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import chalk from 'chalk';
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from 'joi';
 import bcrypt from 'bcrypt';
@@ -80,9 +80,70 @@ app.post('/login', async (req,res) => {
         }
 
     } catch (error) {
+        
         return res.status(500).send("Deu ruim");
    
     }
+})
+
+app.get('/input', async (req,res) => {
+
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    const session = await db.collection('contas').findOne({ token });
+
+    if (!session) {
+        return res.sendStatus(401);
+    }
+    
+    try {
+    
+        const contas = await db.collection('contas').find({ user1Id: new ObjectId(session.user1Id) }).toArray();
+    
+        res.send(contas);
+        res.send(authorization);
+
+    } catch (error) {
+        
+        return res.status(500).send("Deu ruim");
+   
+    }
+
+})
+
+app.post('/input', async (req,res) => {
+
+    const conta = req.body;
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+
+    const contaSchema = joi.object({
+        valor: joi.number().required(),
+        descricao: joi.string().required()
+    })
+
+    const { error } = contaSchema.validate(conta);
+
+    if (error) {
+        return res.sendStatus(422);
+    }
+
+    try {
+        const session = await db.collection('contas').findOne({ token });
+
+        if (!session) {
+            return res.sendStatus(401);
+        }
+    
+        await db.collection('contas').insertOne({ ... conta, user1Id: session.user1Id });
+        return res.status(201).send('Valor adicionado com sucesso');
+
+    } catch (error) {
+        
+        return res.status(500).send("Deu ruim");
+   
+    }
+
 })
 
 app.listen(PORT, () => {
